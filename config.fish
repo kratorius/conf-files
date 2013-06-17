32ec2fa7004839b856a49092c37115af97a131fe
Ivan Giuliani <giuliani.v@gmail.com>
2013-06-17 13:08:01 +0200

    Added fish config file

---
+++ config.fish
@@ -0,0 +1,30 @@
+# Remove welcome message
+set fish_greeting
+
+# add custom scripts to $PATH
+set PATH $PWD/bin $PATH
+
+# I hate gnome keyring and I don't want to use it ever again, however
+# gnome thinks I have to use it and prints stupid warnings all over my
+# output. Just get rid of it.
+set -e GNOME_KEYRING_CONTROL
+
+function prompt_pwd --description 'Print the current working directory, NOT shortened to fit the prompt'
+  printf '%s:' (hostname|cut -d . -f 1)
+  if test "$PWD" != "$HOME"
+    printf "%s" (echo $PWD|sed -e 's|/private||' -e "s|^$HOME|~|")
+  else
+    echo '~'
+  end
+  set_color normal
+end
+
+function fish_prompt
+  set_color $fish_color_cwd
+  echo -n (prompt_pwd)
+  set color normal
+
+  echo -n (__fish_git_prompt)
+  set_color normal
+  echo '$ '
+end
