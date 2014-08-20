a6cb7e39636ed76104fdc56816346ebb8d8435b1
Ivan Giuliani <giuliani.v@gmail.com>
2014-08-20 17:38:48 +0200

    Split menu from main config file

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -116,27 +116,7 @@ end
 -- }}}
 
 -- {{{ Menu
-mymainmenu = awful.menu({
-  items = {
-    { "Terminal", terminal },
-    { "Irssi", "/home/ivan/bin/irssi.sh" },
-    { "Chrome", "google-chrome" },
-    { "Firefox", "firefox" },
-    { "Gimp", "gimp" },
-    { "IntelliJ IDEA", "/home/ivan/.system/idea-IU/bin/idea.sh" },
-    { "PyCharm", "/home/ivan/.system/pycharm/bin/pycharm.sh" },
-    { "PHPStorm", "/home/ivan/.system/phpstorm/bin/phpstorm.sh" },
-    { "RubyMine", "/home/ivan/.system/rubymine/bin/rubymine.sh" },
-    { "Sublime", "/home/ivan/.system/SublimeText2/sublime_text" },
-    { "Android Studio", "/home/ivan/.system/android-studio/bin/studio.sh" },
-    { "VirtualBox", "virtualbox" },
-    { "Transmission", "transmission-gtk" },
-		{ "Suspend", "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" /org/freedesktop/UPower org.freedesktop.UPower.Suspend"},
-		{ "Hibernate", "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" /org/freedesktop/UPower org.freedesktop.UPower.Hibernate"},
-    { "Logout", awesome.quit }
-  }
-})
-
+require("menu") -- loads mymainmenu
 mylauncher = awful.widget.launcher({ menu = mymainmenu })
 
 -- Menubar configuration
