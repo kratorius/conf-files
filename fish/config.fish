4ebdd998092e01e4a3cf3efb4b7f6f308fcf3202
Ivan Giuliani <ivan@lumi.do>
2014-11-10 12:12:21 +0000

    Support termcap colors

--- fish/config.fish
+++ fish/config.fish
@@ -53,3 +53,12 @@ set -g VIRTUALFISH_COMPAT_ALIASES # uncomment for virtualenvwrapper-style comman
 . $HOME/development/virtualfish/auto_activation.fish
 . $HOME/development/virtualfish/global_requirements.fish
 . $HOME/development/virtualfish/projects.fish
+
+# Termcap colors
+set -x LESS_TERMCAP_mb (printf "\e[01;31m")
+set -x LESS_TERMCAP_md (printf "\e[01;31m")
+set -x LESS_TERMCAP_me (printf "\e[0m")
+set -x LESS_TERMCAP_se (printf "\e[0m")
+set -x LESS_TERMCAP_so (printf "\e[01;44;33m")
+set -x LESS_TERMCAP_ue (printf "\e[0m")
+set -x LESS_TERMCAP_us (printf "\e[01;32m")
